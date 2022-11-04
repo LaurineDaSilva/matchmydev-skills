5b72bce000a300a7cffa05306ac50ec3905b33f2
LaurineDS <laurine.dasilva0@gmail.com>
2022-11-04 15:14:14 +0100

    #76  externalize h1 of index.html

--- front-vanilla/src/init.js
+++ front-vanilla/src/init.js
@@ -1,7 +1,9 @@
 import { initMessages } from "./messages.js";
+import { renderh1 } from "./components/h1.js";
 import { renderForm } from "./components/form.js";
 import { renderHeader } from "./components/header.js";
 
 await initMessages();
+renderh1();
 renderForm();
 renderHeader();
