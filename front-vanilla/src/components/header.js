e6ec6b0a351cb38c419a5c51daaff9b31244c7aa
LaurineDS <laurine.dasilva0@gmail.com>
2022-11-04 13:02:08 +0100

    #69 Creates header component

---
+++ front-vanilla/src/components/header.js
@@ -0,0 +1,30 @@
+import { messages } from "../messages.js";
+
+function renderHeader() {
+    const headerContent = `
+        <nav class="navbar bg-black navbar-dark container-xl navbar-expand-md">
+            <a class="navbar-brand text-white mb-0 ">${messages.logo.firstWord}<span class="logo-span">${messages.logo.secondWord}</span>${messages.logo.thirdWord}</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav text-white ">
+                    <li class="nav-item ms-3">
+                        <a href="#" class="nav-link text-white"> Profiles </a>
+                    </li>
+                    <li class="nav-item ms-3">
+                        <a href="#" class="nav-link text-white"> Projects </a>
+                    </li>
+                    <li class="nav-item ms-3">
+                        <a href="#" class="nav-link text-white"> Sign In </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        `
+
+    const header = document.getElementById("header");
+    header.innerHTML = headerContent;
+}
+
+export { renderHeader };
--- front-vanilla/src/router.js
+++ front-vanilla/src/router.js
@@ -1,3 +1,5 @@
 import { renderForm } from "./components/form.js";
+import { renderHeader } from "./components/header.js";
 
 renderForm();
+renderHeader();
