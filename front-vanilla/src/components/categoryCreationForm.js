ec8c4beeeea527b58c20d30c7569c811efb6b8cf
LaurineDS <laurine.dasilva0@gmail.com>
2022-11-07 12:00:08 +0100

    #78 refactoring: renamed form.js, its functions, variables and exports, so that it is clear that is it the form for the creation of new skill categories, as we are going to create a new form for the creation of new skills

--- front-vanilla/src/components/categoryCreationForm.js
+++ front-vanilla/src/components/categoryCreationForm.js
@@ -1,29 +1,29 @@
 import { messages } from "../messages.js";
 
-function renderForm() {
-    const formContent = `
+function renderCategoryCreationForm() {
+    const categoryCreationFormContent = `
         <div class="mb-3">
-            <label for="category-name" class="form-label">${messages.form.name.label}</label>
+            <label for="category-name" class="form-label">${messages.categoryCreationForm.name.label}</label>
             <input type="text" id="category-name" class="form-control" name="category-name" required minlength="1" maxlength="100">
-            <p class="form-text">${messages.form.name.helpText}</p>
+            <p class="form-text">${messages.categoryCreationForm.name.helpText}</p>
         </div>
 
         <div class="mb-3">
-            <p class="form-label">${messages.form.kind.label}</p>
+            <p class="form-label">${messages.categoryCreationForm.kind.label}</p>
             <select class="form-select" required>
-                <option value="" selected>${messages.form.kind.valueDefault}</option>
-                <option value="1">${messages.form.kind.value1}</option>
-                <option value="2">${messages.form.kind.value2}</option>
+                <option value="" selected>${messages.categoryCreationForm.kind.valueDefault}</option>
+                <option value="1">${messages.categoryCreationForm.kind.value1}</option>
+                <option value="2">${messages.categoryCreationForm.kind.value2}</option>
             </select>
         </div>
 
         <div class="mb-3">
-            <label for="category-color" class="form-label">${messages.form.color.label}</label>
+            <label for="category-color" class="form-label">${messages.categoryCreationForm.color.label}</label>
             <div class="input-group">
                 <input type="text" id="category-color-field" class="form-control w-75" name="category-color" required minlength="1" maxlength="7">
                 <input type="color" id="category-color-button" class="form-control form-control-color w-25" name="category-color"  value= "#FF0000" required>
             </div>
-                <p class="form-text">${messages.form.color.helpText}</p>
+                <p class="form-text">${messages.categoryCreationForm.color.helpText}</p>
         </div>              
 
         <div class="mb-3">
@@ -32,7 +32,7 @@ function renderForm() {
     ;
 
     const form = document.getElementById("form");
-    form.innerHTML = formContent;
+    form.innerHTML = categoryCreationFormContent;
 }
 
-export { renderForm };
+export { renderCategoryCreationForm };
--- front-vanilla/src/init.js
+++ front-vanilla/src/init.js
@@ -1,9 +1,9 @@
 import { initMessages } from "./messages.js";
 import { renderh1 } from "./components/h1.js";
-import { renderForm } from "./components/form.js";
+import { renderCategoryCreationForm } from "./components/categoryCreationForm.js";
 import { renderHeader } from "./components/header.js";
 
 await initMessages();
 renderh1();
-renderForm();
+renderCategoryCreationForm();
 renderHeader();
