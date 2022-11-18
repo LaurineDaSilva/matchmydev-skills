1f2f736d57247c1852d0ed12934a4d2f2442edb4
Lindsay <lindsay.dev33@gmail.com>
2022-11-18 17:34:53 +0100

    first commit spring

---
+++ skills-api/src/main/java/co/simplon/matchmydev/skills/SkillsApiApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.matchmydev.skills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillsApiApplication.class, args);
+	}
+
+}
