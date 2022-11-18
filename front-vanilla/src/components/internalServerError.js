0f34b387200414b7dbb572cbc73f3d3426fea352
LaurineDS <laurine.dasilva0@gmail.com>
2022-11-18 15:32:09 +0100

    #17 Turn InternalServerError into class

--- front-vanilla/src/components/internalServerError.js
+++ front-vanilla/src/components/internalServerError.js
@@ -1,13 +1,15 @@
 import { } from "";
 
-function renderError500() {
-    const contentError500 = `
-        <h1>Error 500 - Internal Server Error</h1>
+
+export default class InternalServerError {
+    render() {
+        const contentError500 = `
+            <h1>Error 500 - Internal Server Error</h1>
             <img src="" alt="" class="img-fluid">
-    `;
+        `;
 
     const error500 = document.getElementById("form");
     error500.innerHTML = contentError500;
 };
 
-export { renderError500 };
+}
