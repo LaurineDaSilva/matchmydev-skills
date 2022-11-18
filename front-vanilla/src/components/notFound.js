bbbbf9868513ec98b570a2b1ad5330c8e53bbd42
LaurineDS <laurine.dasilva0@gmail.com>
2022-11-18 15:36:26 +0100

    #17 turn NotFound (error 404) into class

--- front-vanilla/src/components/notFound.js
+++ front-vanilla/src/components/notFound.js
@@ -1,13 +1,15 @@
 import { } from "";
 
-function renderError404() {
-    const contentError404 = `
-        <h1>Error 404 - Not Found</h1>
+export default class NotFound {
+
+    render() {
+        const contentError404 = `
+            <h1>Error 404 - Not Found</h1>
             <img src="" alt="" class="img-fluid">
-    `;
+        `;
 
     const error404 = document.getElementById("form");
     error404.innerHTML = contentError404;
-};
+    };
 
-export { renderError404 };
+}
