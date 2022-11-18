3780116a20e3d83d480fd80d6841aea215acc11e
LaurineDS <laurine.dasilva0@gmail.com>
2022-11-18 15:47:36 +0100

    #17 fix NotFound class

--- front-vanilla/src/components/notFound.js
+++ front-vanilla/src/components/notFound.js
@@ -7,7 +7,7 @@ export default class NotFound extends BaseComponent {
     }
 
     template() {
-        return contentError404 = `
+        return `
             <h1>Error 404 - Not Found</h1>
             <img src="" alt="" class="img-fluid">
         `;
