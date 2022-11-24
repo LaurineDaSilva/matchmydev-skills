7c37078b4c37b0759c1804dd391e07a4093d82b2
Lindsay <lindsay.dev33@gmail.com>
2022-11-24 12:59:07 +0100

    #111 Update JSON for CategorySearchComponent

--- front-vanilla/src/components/CategorySearchComponent.js
+++ front-vanilla/src/components/CategorySearchComponent.js
@@ -11,23 +11,23 @@ export default class CategorySearchComponent extends BaseIterableComponent{
 
     template(){
         return `
-        <h1>Search for a skill category</h1>
+        <h1>${this.msg.categorySearchForm.h1}</h1>
         <form class="mb-3 mt-4">
-            <label for="name" class="form-label">Skill category name</label>
+            <label for="name" class="form-label">${this.msg.categorySearchForm.label}</label>
             <div class="input-group">
                 <input id="name" name="name" type="text" class="form-control" required/>
                 <span class="input-group-text"><i class="bi bi-search"></i></span>
                 <button class="btn btn-outline-dark" type="submit">Search</button>
             </div>
-            <p class="form-text">Please enter part of or full name of the skill category, will find any match containing input text.</p>   
+            <p class="form-text">${this.msg.categorySearchForm.helpText}</p>   
         </form>        
-        <h2>Categories</h2>
+        <h2>${this.msg.categorySearchForm.categories.h2}</h2>
         <div class=" w-100 table-responsive-lg">
             <table class="table table-bordered table-hover">
                 <thead class="table-labels-background">
-                    <th scope="col" class="col">Name</th>
-                    <th scope="col" class="col">Kind</th>
-                    <th scope="col" class="col">Color</th>
+                    <th scope="col" class="col">${this.msg.categorySearchForm.categories.firstColumn}</th>
+                    <th scope="col" class="col">${this.msg.categorySearchForm.categories.secondColumn}</th>
+                    <th scope="col" class="col">${this.msg.categorySearchForm.categories.thirdColumn}</th>
                 </thead>
                 <tbody id="elements">
                 </tbody>
