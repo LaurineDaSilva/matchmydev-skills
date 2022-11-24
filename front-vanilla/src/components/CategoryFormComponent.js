ea01435335969e238a48d51b7c4683f5cd1fc1ca
LaurineDS <laurine.dasilva0@gmail.com>
2022-11-24 12:05:16 +0100

    #64 Create SkillFormComponent and update Router accordingly

--- front-vanilla/src/components/CategoryFormComponent.js
+++ front-vanilla/src/components/CategoryFormComponent.js
@@ -13,14 +13,14 @@ export default class CategoryFormComponent extends BaseComponent {
                     <div class="mb-3">
                         <label for="category-name" class="form-label">${this.msg.categoryForm.name.label}</label>
                         <input type="text" id="category-name" class="form-control" name="category-name" required minlength="1" maxlength="100" oninvalid="this.setCustomValidity('Required field.')"
-                        oninput="this.setCustomValidity('')"">
+                        oninput="this.setCustomValidity('')">
                         <p class="form-text">${this.msg.categoryForm.name.helpText}</p>
                     </div>
 
                     <div class="mb-3">
                         <p class="form-label">${this.msg.categoryForm.kind.label}</p>
                         <select class="form-select" required oninvalid="this.setCustomValidity('Required field.')"
-                        oninput="this.setCustomValidity('')"">
+                        oninput="this.setCustomValidity('')">
                             <option value="" selected>${this.msg.categoryForm.kind.valueDefault}</option>
                             <option value="1">${this.msg.categoryForm.kind.value1}</option>
                             <option value="2">${this.msg.categoryForm.kind.value2}</option>
@@ -31,7 +31,7 @@ export default class CategoryFormComponent extends BaseComponent {
                         <label for="category-color" class="form-label">${this.msg.categoryForm.color.label}</label>
                         <div class="input-group">
                             <input type="text" id="category-color-field" class="form-control w-75" name="category-color" required minlength="1" maxlength="8" oninvalid="this.setCustomValidity('Required field.')"
-                            oninput="this.setCustomValidity('')"">
+                            oninput="this.setCustomValidity('')">
                             <input type="color" id="category-color-button" class="form-control form-control-color w-25" name="category-color"  value= "#FF0000" required>
                         </div>
                             <p class="form-text">${this.msg.categoryForm.color.helpText}</p>
--- front-vanilla/src/router.js
+++ front-vanilla/src/router.js
@@ -1,12 +1,23 @@
 import HeaderComponent from "./components/HeaderComponent.js";
 import CategoryFormComponent from "./components/CategoryFormComponent.js";
+import SkillFormComponent from "./components/SkillFormComponent.js";
 
 export default class Router {
 
     static init() {
         const headerComponent = new HeaderComponent();
+        Router.route();
         headerComponent.render();
-        const categoryForm = new CategoryFormComponent;
-        categoryForm.render();
+    }
+
+    static route(event) {
+        const hash = window.location.hash;
+        let component = null;
+        if (hash == "#/category-form" || hash == "") {
+            component = new CategoryFormComponent;
+        } else if (hash == "#/skill-form") {
+            component = new SkillFormComponent();
+        }
+        component.render();
     }
 }
