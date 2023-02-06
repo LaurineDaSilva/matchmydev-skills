f00e53edf58faa92f07ac7dad552357ca5d4aedf
Lisa Munsy <stillailleurs@gmail.com>
2023-02-06 15:28:26 +0100

    Added styles and favicon in assets, added Navbar component

--- front-vue/src/App.vue
+++ front-vue/src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
-  <header>
-   
+  <header class="bg-black  fixed-top mb-3" id="header">
+   <Navbar/>
   </header>
 
   <RouterView />
---
+++ front-vue/src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <nav class="navbar bg-black navbar-dark container-xl navbar-expand-md">
+        <a class="navbar-brand text-white mb-0 ">match<span class="logo-span">my</span>dev</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav text-white ">
+                <li class="nav-item ms-3">
+                    <a href="#/category-form" class="nav-link text-white"> Create a skill category </a>
+                </li>
+                <li class="nav-item ms-3">
+                    <a href="#/skill-form" class="nav-link text-white"> Create a new skill </a>
+                </li>
+                <li class="nav-item ms-3">
+                    <a href="#/category-list" class="nav-link text-white"> Search for skill categories </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+</template>
