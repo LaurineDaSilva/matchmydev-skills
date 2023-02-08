e79c2908e1f3647bbb33120b81f31e17b183a8fa
LaurineDS <laurine.dasilva0@gmail.com>
2023-02-08 11:33:15 +0100

    change BASEURL from axiosConfig plugin. Before, it was hardcoded in the file. Now, it's externalized to .env.local file, which is git ignored

--- front-vue/src/plugins/axiosConfig.js
+++ front-vue/src/plugins/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default {
   install: (app, options) => {
     const http = axios.create({
-      baseURL: 'http://localhost:8080',
+      baseURL: import.meta.env.VITE_API_BASE_URL,
     });
 
     app.config.globalProperties.$axios = http;
